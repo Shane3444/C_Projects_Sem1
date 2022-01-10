66ab0f07cd0f661f7084f7820ddaf4fd102ce113
Shane3444 <97429900+Shane3444@users.noreply.github.com>
2022-01-10 12:55:55 +0530

    Update program2.c

--- program2.c
+++ program2.c
@@ -2,5 +2,25 @@
 
 int main(void)
 {
+    float x, y, z, a, b, c, ans;
     
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+    printf("Enter the value of c: ");
+    scanf("%d", &c);
+    
+    printf("Equation: %d * x + %d * y + %d * z")
+    
+    printf("Enter the value of x: ");
+    scanf("%d", &x);
+    printf("Enter the value of y: ");
+    scanf("%d", &y);
+    printf("Enter the value of z: ");
+    scanf("%d", &z);
+    
+    ans = a * x + b * y + c * z;
+    
+    printf("Answer: %d", ans);
 }
